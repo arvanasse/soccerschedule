c163f451e56b3e7e9c92f006679d6a395548bc02
arvanasse <andyvanasse@gmail.com>
2010-09-17 08:07:55 -0400

    Account for 404 error from the url of a ScheduleLink.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,19 +6,27 @@ class Schedule
     def find( team_urls )
       return [ ] if team_urls.empty?
       schedule = team_urls.collect do |team_url_info|
-        doc = Nokogiri::HTML( open(team_url_info[:url]) )
-        rows = doc.css("tr.GameRow").collect do |game_row|
-          game_info = { :date=>game_row['date'].to_date }
-          
-          date_parts = ParseDate.parsedate( game_row['time'] )
-          date_parts[0] = game_info[:date].year
-          date_parts[1] = game_info[:date].month
-          date_parts[2] = game_info[:date].day
+        doc = begin
+          Nokogiri::HTML( open(team_url_info[:url]) )
+        rescue
+          nil
+        end
+        if doc
+          rows = doc.css("tr.GameRow").collect do |game_row|
+            game_info = { :date=>game_row['date'].to_date }
+            
+            date_parts = ParseDate.parsedate( game_row['time'] )
+            date_parts[0] = game_info[:date].year
+            date_parts[1] = game_info[:date].month
+            date_parts[2] = game_info[:date].day
 
-          game_info.merge!( :time => Time.mktime(*date_parts) )
+            game_info.merge!( :time => Time.mktime(*date_parts) )
 
-          game_info.merge!( :field => scrub_text( game_row.css('td.facility a').text ) )
-          game_info.merge( :match => game_row.css('td.tm').collect{|team_link| scrub_text(team_link.text)}.join(' vs ') )
+            game_info.merge!( :field => scrub_text( game_row.css('td.facility a').text ) )
+            game_info.merge( :match => game_row.css('td.tm').collect{|team_link| scrub_text(team_link.text)}.join(' vs ') )
+          end
+        else
+          [ ]
         end
       end
 
