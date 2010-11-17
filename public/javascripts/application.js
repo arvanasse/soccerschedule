6baefa1f9b85c98f304d47dfee37eb97409f8501
Andy Vanasse <andyvanasse@gmail.com>
2010-11-17 07:43:21 -0500

    Replace Hanover Mtg ad image.  Fix login box.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Hook the rel='facebox' in order to wire up facebox
+$('a[rel*=facebox]').livequery(function() {
+    $(this).facebox();
+});
+
+
