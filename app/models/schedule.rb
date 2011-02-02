eb42df6d4f24d28a02b4c78dd87f02e777663cfd
Andy Vanasse <andyvanasse@gmail.com>
2011-02-01 22:07:53 -0500

    Work around presence/absence of ParseDate in ruby 1.8.7/1.9.1

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,7 +1,6 @@
 class Schedule
   require 'nokogiri'
   require 'open-uri'
-  require 'date'
 
   class << self
     def find( team_urls )
@@ -16,12 +15,15 @@ class Schedule
           rows = doc.css("tr.GameRow").collect do |game_row|
             game_info = { :date=>game_row['date'].to_date }
             
-            date_parts = ParseDate.parsedate( game_row['time'] )
-            date_parts[0] = game_info[:date].year
-            date_parts[1] = game_info[:date].month
-            date_parts[2] = game_info[:date].day
-
-            game_info.merge!( :time => Time.mktime(*date_parts) )
+            if Rails.env=='production'
+              game_info.merge!( :time => Time.parse("#{game_info[:date].strftime('%Y-%m-%d')} #{game_row['time']}") )
+            else
+              date_parts = ParseDate.parsedate( game_row['time'] )
+              date_parts[0] = game_info[:date].year
+              date_parts[1] = game_info[:date].month
+              date_parts[2] = game_info[:date].day
+              game_info.merge!( :time => Time.mktime(*date_parts) )
+            end
 
             game_info.merge!( :field => scrub_text( game_row.css('td.facility a').text ) )
             game_info.merge( :match => game_row.css('td.tm').collect{|team_link| scrub_text(team_link.text)}.join(' vs ') )
