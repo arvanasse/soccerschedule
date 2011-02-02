59be9db27e13fd916ce46c1bd605bc7481ed4a80
Andy Vanasse <andyvanasse@gmail.com>
2011-02-01 22:30:09 -0500

    Reformat scraped dates for the expected format of Date.parse

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -14,16 +14,20 @@ class Schedule
         if doc
           rows = doc.css("tr.GameRow").collect do |game_row|
             RAILS_DEFAULT_LOGGER.info "Date read: #{game_row['date']}"
-            game_info = { :date=>game_row['date'].to_date }
+            game_info = {  }
             
             if Rails.env=='production'
-              game_info.merge!( :time => Time.parse("#{game_info[:date].strftime('%Y-%m-%d')} #{game_row['time']}") )
+              parts = game_info[:date].split(/\//)
+              game_date = parts.unshift( parts.pop ).join('-')
+              game_info.merge!( :date => game_date, :time => Time.parse("#{game_date.strftime('%Y-%m-%d')} #{game_row['time']}") )
             else
+              game_date = game_row['date'].to_date
+
               date_parts = ParseDate.parsedate( game_row['time'] )
-              date_parts[0] = game_info[:date].year
-              date_parts[1] = game_info[:date].month
-              date_parts[2] = game_info[:date].day
-              game_info.merge!( :time => Time.mktime(*date_parts) )
+              date_parts[0] = game_date.year
+              date_parts[1] = game_date.month
+              date_parts[2] = game_date.day
+              game_info.merge!( :date => game_date, :time => Time.mktime(*date_parts) )
             end
 
             game_info.merge!( :field => scrub_text( game_row.css('td.facility a').text ) )
