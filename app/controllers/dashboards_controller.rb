740bb0a54c409b5f03a2612969d9955910e02931
Andy Vanasse <andyvanasse@gmail.com>
2011-02-25 12:41:08 -0500

    Still debugging nil.to_sym

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,8 @@ class DashboardsController < ApplicationController
   layout 'mobile'
 
   def show
+    logger.info "The environment is #{Rails.env}"
+
     respond_to do |format|
       format.html
     end
