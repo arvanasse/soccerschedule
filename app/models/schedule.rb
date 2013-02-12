1cbbf623a9a9ab0ca0f260f0bcf9bf9b263aafb9
arvanasse <andyvanasse@gmail.com>
2013-02-12 11:47:33 -0500

    Display collected data.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -40,8 +40,9 @@ class Schedule
             game_info.merge!( :field => scrub_text( game_row.css('td span.tmcode').text ) )
 
             game_info.merge!( :match => game_row.css('td.schedtm').collect{|team_link| scrub_text(team_link.text)}.join(' vs ') )
+            Rails.logger.info game_info.inspect
 
-            game_info
+            return game_info
           end
         end
       else
